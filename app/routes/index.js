beca9ac559162532eae37efae760be2483e38542
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-01 17:30:16 +0800

    announcement delete button done

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,13 +4,18 @@ export default Ember.Route.extend({
   model() {
      return Ember.RSVP.hash({
        rentals: this.store.findAll('rental'),
-       reviews: this.store.findAll('review')
+       reviews: this.store.findAll('review'),
+       announcements: this.store.findAll('announcement')
      });
    },
    actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+    destroyAnnouncement(announcement){
+      announcement.destroyRecord();
       this.transitionTo('index');
     }
   }
