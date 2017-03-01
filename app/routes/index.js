da02f1cc2d640aac1440e2daf073473506ce41ed
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-01 15:53:23 +0800

    final

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return Ember.RSVP.hash ({
+     return Ember.RSVP.hash({
        rentals: this.store.findAll('rental'),
        reviews: this.store.findAll('review')
      });
